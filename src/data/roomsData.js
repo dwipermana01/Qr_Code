87caaaf576cb540a262abe339a7808d6ea914569
Dwi_Permana <dwipermana01@gmail.com>
2025-09-17 14:41:57 +0700

    Update icon lab kom

--- src/data/roomsData.js
+++ src/data/roomsData.js
@@ -521,7 +521,7 @@ export const roomsData = {
       { icon: "/icons/computer.svg", name: "Komputer", count: "32 unit" },
       { icon: "/icons/greenboard.svg", name: "Papan Tulis Hijau", count: "1 unit" },
       { icon: "/icons/router.svg", name: "Router", count: "1 unit" },
-      { icon: "/icons/airConditioner.svg", name: "AC", count: "1 unit" }
+      { icon: "/icons/airConditioner.svg", name: "AC", count: "2 unit" }
     ],
     mapImage: "/maps/r030402.jpg",
     legendItems: [
